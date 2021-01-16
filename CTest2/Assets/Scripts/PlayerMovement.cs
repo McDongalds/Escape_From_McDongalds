ba2e494e5352f6b0a1efd4d8b7ab6aad2302a11b
Jcal516 <jasonc16@icloud.com>
2021-01-15 21:34:16 -0700

    vGrav

--- CTest2/Assets/Scripts/PlayerMovement.cs
+++ CTest2/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed;
+<<<<<<< Updated upstream
+=======
+    public float smoothTime;
+    float turnSmoothVelocity;
+    public Transform cam;
+    public float gravity = -9.8f;
+    public float ySpeed = 0f;
+
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         transform.Translate(playerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, playerSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+=======
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(h, 0f, v).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
+
+        }
+        if (controller.isGrounded)
+            ySpeed = 0;
+        if (!controller.isGrounded)
+        {
+            ySpeed += gravity * Time.deltaTime;
+            Vector3 vGrav = new Vector3(0f, ySpeed, 0f);
+            controller.Move(vGrav * Time.deltaTime);
+        }
+        
+        
+>>>>>>> Stashed changes
     }
 }
