cbfc3b0c81883d44189f571e9327afd1a81e004b
Jackson <73448032+jddaggett1@users.noreply.github.com>
2021-03-21 11:51:50 -0700

    Menu

---
+++ CTest2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //loads the Scene with an index that is 1 greater than the current index
+        //Menu scene index = 0, game scene index = 1. Can be scene in build settings menu.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //Will only work after build
+        Application.Quit();
+    }
+}
