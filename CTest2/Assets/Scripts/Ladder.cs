16650dd7e2ed582dbcb580dfc72d7b274e397afc
Jackson <73448032+jddaggett1@users.noreply.github.com>
2021-04-28 21:07:04 -0700

    LADDERS

---
+++ CTest2/Assets/Scripts/Ladder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public Transform playerController;
+    public float ladderHeight = 10f;
+    private bool onLadder; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        onLadder = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ladder")
+        {
+            onLadder = !onLadder;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Ladder")
+        {
+            onLadder = !onLadder;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (onLadder)
+        {
+            playerController.transform.position += Vector3.up * ladderHeight;
+        }
+    }
+}
