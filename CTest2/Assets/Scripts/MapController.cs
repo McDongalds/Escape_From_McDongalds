6f63b2f420eeaca13fd2587a6df228e6bfefe5ad
Jackson <73448032+jddaggett1@users.noreply.github.com>
2021-04-28 19:30:21 -0700

    enemy AI

---
+++ CTest2/Assets/Scripts/MapController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject MapObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MapObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("m")){
+            MapObject.SetActive(true);
+        }
+        else{
+            MapObject.SetActive(false);
+        }
+        //gameObject.SetActive(true);
+    }
+}
---
+++ CTest2/Assets/Scripts/PatrolAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolAI : MonoBehaviour
+{   
+    public Transform[] waypoints;
+    public int speed;
+
+    private int waypointIndex;
+    private float distance;
+
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    void FixedUpdate()
+    {
+        distance = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if(distance < 1f)
+        {
+            IncrementIndex();
+        }
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncrementIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+}
