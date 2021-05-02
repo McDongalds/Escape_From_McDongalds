f3adde82cf19cc6b98b8179f128fdd64ec0372de
Jcal516 <jasonc16@icloud.com>
2021-05-02 13:17:32 -0700

    Added Dome HItboxes & Gave Walking Animation

--- CTest2/Assets/PlayerAnimator.cs
+++ CTest2/Assets/PlayerAnimator.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    Animator animation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //GetComponent<Animator>().SetFloat("Speed", 1f);
+        if (Input.GetAxisRaw("Vertical")>0)
+            animation.SetFloat("Speed", 0.9f);
+        else
+            animation.SetFloat("Speed", 0f);
     }
 }
