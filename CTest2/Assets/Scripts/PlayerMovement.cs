aeb7593fe3241b36d2d0b63a20292765e21fcea0
Jcal516 <jasonc16@icloud.com>
2021-05-05 20:53:34 -0700

    build

--- CTest2/Assets/Scripts/PlayerMovement.cs
+++ CTest2/Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         instance = this;
     }
 
